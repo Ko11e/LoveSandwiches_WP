e2aec518e822a4b2a53bef641da152e1a3a1acb4
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-03-05 08:18:20 +0000

    Collect sales data for last 5 markets

--- run.py
+++ run.py
@@ -85,7 +85,20 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
 
     return surplus_data
+def get_last_5_entres_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
 
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
 
 def main():
     """
@@ -99,4 +112,6 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+
+sales_ columns = get_last_5_entres_sales()
